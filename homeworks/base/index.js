83d8f1d33ee3f610c5ab7861903d848ba0b3bad6
SergioKovalenko <sergey.koval.19@gmail.com>
2023-12-03 18:11:57 +0100

    type hw day1

--- homeworks/base/index.js
+++ homeworks/base/index.js
@@ -4,30 +4,30 @@
 several lines */
 
 
-let username = prompt('What is your name?');
+// let username = prompt('What is your name?');
 
 const message = 'Hello, ' + username;
 
 console.log(message);
 
 
-let userAge = prompt('How old are u?');
+// let userAge = prompt('How old are u?');
 
 console.log(userAge);
 
 
-let gender = prompt('What is your gender?');
+// let gender = prompt('What is your gender?');
 
 console.log(gender);
 
 
-let civilianStatus = prompt('What is your civilian status?');
+// let civilianStatus = prompt('What is your civilian status?');
 
 console.log(civilianStatus);
 
 //
 
-let userYear = prompt('What is your birth year?');
+// let userYear = prompt('What is your birth year?');
 
 const age = 2023 - userYear;
 console.log('user age is ', age);
---
+++ homeworks/types/index.js
@@ -0,0 +1,22 @@
+let str1 = 0.1;
+
+let str2 = 0.2;
+
+const result = str1 + str2;
+
+
+console.log(result.toFixed(1));
+
+// 
+
+let str = "1";
+
+let num = 2;
+
+let num2 = Number.parseInt(str);
+
+console.log(typeof (num2));
+
+const result2 = str + num2;
+
+console.log(result2);
