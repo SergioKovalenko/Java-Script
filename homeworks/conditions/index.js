de00cbaa4c24db6e0eaaa73b2512e08649e9248d
SergioKovalenko <sergey.koval.19@gmail.com>
2023-12-08 22:48:00 +0100

    cycles hw

---
+++ homeworks/conditions/index.js
@@ -0,0 +1,84 @@
+
+// const age = prompt("Вкажіть свій вік");
+
+// if (age >= 1 && age <= 11) {
+//     console.log("Ви є дитиною.");
+// }
+
+// else if (age >= 12 && age <= 17) {
+//     console.log("Ви є підлітком.");
+// }
+
+// else if (age >= 18 && age <= 59) {
+//     console.log("Ви є дорослим.");
+// }
+
+// else if (age >= 60) {
+//     console.log("Ви є пенсіонером.");
+// }
+
+// else {
+//     console.log("Невірно введені дані.");
+// }
+
+//
+
+// const num = prompt('Введіть число від 0 до 9');
+
+// if (num >= 1 && num <= 1) {
+//     console.log("Ваш спецсимвол це !");
+// }
+
+// else if (num >= 2 && num <= 2) {
+//     console.log("Ваш спецсимвол це @");
+// }
+
+// else if (num >= 3 && num <= 3) {
+//     console.log("Ваш спецсимвол це #");
+// }
+
+// else if (num >= 4 && num <= 4) {
+//     console.log("Ваш спецсимвол це $");
+// }
+
+// else if (num >= 5 && num <= 5) {
+//     console.log("Ваш спецсимвол це %");
+// }
+
+// else if (num >= 6 && num <= 6) {
+//     console.log("Ваш спецсимвол це ^");
+// }
+
+// else if (num >= 7 && num <= 7) {
+//     console.log("Ваш спецсимвол це &");
+// }
+
+// else if (num >= 8 && num <= 8) {
+//     console.log("Ваш спецсимвол це *");
+// }
+
+// else if (num >= 9 && num <= 9) {
+//     console.log("Ваш спецсимвол це (");
+// }
+
+// else if (num >= 0 && num <= 0) {
+//     console.log("Ваш спецсимвол це )");
+// }
+
+//
+
+// const sum = prompt('Введіть суму вашої покупки');
+
+// if (sum >= 200 && sum <= 300) {
+//     console.log("Знижка для вас буде складати 3% ");
+// }
+
+// else if (sum >= 300 && sum <= 500) {
+//     console.log("Знижка для вас буде складати 5%");
+// }
+
+// else if (sum >= 500) {
+//     console.log("Знижка для вас буде складати 7%");
+// }
+
+
--- homeworks/types/index.js
+++ homeworks/types/index.js
@@ -1,33 +1,69 @@
-let str1 = 0.1;
+// let str1 = 0.1;
 
-let str2 = 0.2;
+// let str2 = 0.2;
 
-const result = str1 + str2;
+// const result = str1 + str2;
 
-console.log(result.toFixed(1));
+// console.log(result.toFixed(1));
 
-// 
+// //
 
-let a = "1";
+// let a = "1";
 
-let b = 2;
+// let b = 2;
 
-console.log(parseInt(a) + b);  // Пeреведення а зi string in number
+// console.log(parseInt(a) + b);  // Пeреведення а зi string in number
 
-// 
+//
 
-function amountFiles(sizeFlash, sizeFile) {
-    // Переведення обсягу флешки в Мб
-    sizeFlash *= 1024;
+// function amountFiles(sizeFlash, sizeFile) {
+//     // Переведення обсягу флешки в Мб
+//     sizeFlash *= 1024;
 
-    // Обчислення кількості файлів
-    const amount = Math.floor(sizeFlash / sizeFile);
+//     // Обчислення кількості файлів
+//     const amount = Math.floor(sizeFlash / sizeFile);
 
-    return amount;
-}
+//     return amount;
+// }
 
-const sizeFlashGB = parseFloat(prompt("Введіть обсяг флешки в Гб:")); // Запит на введення обсягу флешки
-const sizeFileMB = 820; // Розмір файлу в Мб
+// const sizeFlashGB = parseFloat(prompt("Введіть обсяг флешки в Гб:")); // Запит на введення обсягу флешки
+// const sizeFileMB = 820; // Розмір файлу в Мб
 
-const amount = amountFiles(sizeFlashGB, sizeFileMB);
-console.log(`Кількість файлів розміром ${sizeFileMB} Мb, які поміщаються на флешці обсягом ${sizeFlashGB} Гб: ${amount}`);
+// const amount = amountFiles(sizeFlashGB, sizeFileMB);
+// console.log(`Кількість файлів розміром ${sizeFileMB} Мb, які поміщаються на флешці обсягом ${sizeFlashGB} Гб: ${amount}`);
+
+//
+
+// function amountChoco(sumMonay, priceChoco) {
+//     // Обчислення кількості шоколадок
+//     const amount = Math.floor(sumMonay / priceChoco);
+
+//     // Обчислення залишку грошей
+//     const rest = sumMonay % priceChoco;
+
+//     return [amount, rest];
+// }
+
+// const sumMonay = parseFloat(prompt("Введіть суму грошей в гаманці:"));
+// const priceChoco = parseFloat(prompt("Введіть ціну однієї шоколадки:"));
+
+// const [amount, rest] = amountChoco(sumMonay, priceChoco);
+// console.log(`За ${sumMonay} грн можна купити ${amount} шоколадок і залишиться здачі ${rest} грн.`);
+
+//
+
+// const num = prompt('Введіть трьохзначне число');
+
+// let turnNum = 0;
+
+// if (num.length === 3) {
+//     turnNum = num % 10; // Отримання першої цифри числа
+//     turnNum = turnNum * 10 + Math.floor((num / 10) % 10); // Отримання другої цифри числа
+//     turnNum = turnNum * 10 + Math.floor(num / 100); // Отримання третьої цифри числа
+
+//     console.log(`Число задом наперед: ${turnNum}`);
+// } else {
+//     console.log("Будь ласка, введіть тризначне число.");
+// }
+
+
